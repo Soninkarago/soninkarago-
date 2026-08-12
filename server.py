b7ff2cb5c449de6cd18ac2b20f8da11a4f0ad819
Soninkarago <issa.coulibaly.59@hotmail.com>
2026-08-12 22:34:08 +0000

    Add tracking token generation to server.py

--- server.py
+++ server.py
@@ -1012,7 +1012,7 @@ class App(SimpleHTTPRequestHandler):
                 "SG-"
                 + secrets.token_hex(6).upper()
             )
-
+tracking_token = secrets.token_urlsafe(32)
             client_name = str(
                 data.get(
                     "client_name",
